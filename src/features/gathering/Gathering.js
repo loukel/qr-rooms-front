c5cbac048f4f5fc31470bff1409a31d936c3c8f1
loukel <louis.kelly1@hotmail.com>
2021-10-08 10:41:21 +0100

    Disabled react-hooks exhaustive-deps

--- src/features/gathering/Gathering.js
+++ src/features/gathering/Gathering.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from 'react'
 import { useHistory, useParams } from 'react-router'
 import Header from '../../components/header/Header'
